c08a72654effa81462a2d23b88ba37f4092ab739
khaledf <khaled.ali511@gmail.com>
2023-05-23 21:47:00 +0300

    Getter and Setter for Username

--- src/User.java
+++ src/User.java
@@ -6,4 +6,12 @@ public class User {
         this.username = username;
         this.password = password;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
